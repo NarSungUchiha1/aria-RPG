1b4e95efe8b55da357e1d339f9e03699858c9559
NarSung Uchiha <barry62630@gmail.com>
2026-06-09 04:26:24 +0000

    sooner or later Im gonna run outta stuf- see what I mean

--- src/commands/equip.js
+++ src/commands/equip.js
@@ -13,8 +13,7 @@ module.exports = {
 
         try {
             const [items] = await db.execute(
-                "SELECT * FROM inventory WHERE player_id=? AND item_name NOT LIKE '%Void Shard%' ORDER BY id    
-                [userId]
+                "SELECT * FROM inventory WHERE player_id=? AND item_name NOT LIKE '%Void Shard%' ORDER BY id",[userId]
             );
             const item = items[idx];
             if (!item) return msg.reply(
